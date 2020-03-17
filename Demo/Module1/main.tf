90b2855c1d015d59d90c1123d6a3763db2e6d80a
Nidhish <42999787+nidhisht@users.noreply.github.com>
2020-03-17 17:46:45 +0530

    Masked the subscription, app id, secret etc

--- Demo/Module1/main.tf
+++ Demo/Module1/main.tf
@@ -2,10 +2,10 @@ provider "azurerm" {
   # Whilst version is optional, we /strongly recommend/ using it to pin the version of the Provider being used
   version = "=1.44.0"
 
-  subscription_id = "3a42fcdd-f070-426f-8464-e64415058bcc"
-  client_id       = "bc7ac24b-448b-4446-8983-e8059899f010"
-  client_secret   = "9d4c53c4-2ff9-4c49-8444-ab27e443d58e"
-  tenant_id       = "7cfc78c8-378e-40a6-b974-d6a520ddada6"
+  subscription_id = "00000000-0000-0000-0000-000000000000"
+  client_id       = "00000000-0000-0000-0000-000000000000"
+  client_secret   = "00000000-0000-0000-0000-000000000000"
+  tenant_id       = "00000000-0000-0000-0000-000000000000"
 }
 
 resource "azurerm_resource_group" "terraform-resource-group" {
